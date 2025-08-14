a40ea1dbbbc28ed27f179b22b55577104afedd58
Angel Cervera Roldan <48255007+angelcerveraroldan@users.noreply.github.com>
2025-08-14 12:57:38 +0100

    Basic parser for types

--- calamars/src/parser.rs
+++ calamars/src/parser.rs
@@ -1,23 +1,85 @@
 //! Parser for Calamars
 
-// TOOD: Fix later, import what you need only
-use chumsky::*;
+use chumsky::{input::ValueInput, prelude::*};
 
-enum ClPrimitive {
-    Integer,
-    Float,
-    String,
-    Boolean,
+use crate::token::Token;
+
+trait TokenInput<'a>: ValueInput<'a, Token = Token, Span = SimpleSpan> {}
+impl<'a, I> TokenInput<'a> for I where I: ValueInput<'a, Token = Token, Span = SimpleSpan> {}
+
+struct Ident {
+    src: String,
+    span: SimpleSpan,
 }
 
-enum BinaryOp {
-    Add,
-    Sub,
-    Times,
-    Div,
-    Neg,
+/// Calamars Base Type Instance
+#[derive(Debug, Clone, PartialEq, PartialOrd)]
+pub enum ClLiteral {
+    Integer(i64),
+    Real(f64),
+    String(String),
+    Boolean(bool),
+    Char(char),
+    Array(Vec<Self>),
 }
 
-enum Expression {
-    Literal(ClPrimitive),
+/// Parse the base types
+fn parse_atom<'a, I>() -> impl Parser<'a, I, ClLiteral, extra::Err<Rich<'a, Token>>> + Clone
+where
+    I: TokenInput<'a>,
+{
+    select! {
+        Token::True      => ClLiteral::Boolean(true),
+        Token::False     => ClLiteral::Boolean(false),
+        Token::Int(i)    => ClLiteral::Integer(i),
+        Token::Float(f)  => ClLiteral::Real(f),
+        Token::String(s) => ClLiteral::String(s),
+        Token::Char(c)   => ClLiteral::Char(c),
+    }
+}
+
+/// Parse any base value, including nested arrays
+pub fn base_type_parser<'a, I>()
+-> impl Parser<'a, I, ClLiteral, extra::Err<Rich<'a, Token>>> + Clone
+where
+    I: TokenInput<'a>,
+{
+    recursive(|value| {
+        let arr = value
+            .separated_by(just(Token::Comma))
+            .allow_trailing()
+            .collect::<Vec<_>>()
+            .delimited_by(just(Token::LBracket), just(Token::RBracket));
+
+        choice((parse_atom(), arr.map(ClLiteral::Array)))
+    })
+}
+
+pub enum ClType {
+    /// Basic / standard types such as Int, String, Char, Real, ...
+    /// as well as types that require many segments, such as people.Person
+    Path {
+        segments: Vec<Ident>,
+        total_span: SimpleSpan,
+    },
+    /// An array of some type such as [Int]
+    Array {
+        elem_type: Box<Self>,
+        total_span: SimpleSpan,
+    },
+    /// A function (I1, I2, I3, ...) -> (O1, O2, O3, ...)
+    Func {
+        inputs: Vec<Self>,
+        output: Box<Self>,
+        total_span: SimpleSpan,
+    },
+}
+
+fn parse_cltype_annotation<'a, I>()
+-> impl Parser<'a, I, ClLiteral, extra::Err<Rich<'a, Token>>> + Clone
+where
+    I: TokenInput<'a>,
+{
+    // TODO
+    parse_atom()
 }
