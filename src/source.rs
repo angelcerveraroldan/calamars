7412a4b8d2f04985e9c758d06d96e5769bc81878
Angel Cervera Roldan <48255007+angelcerveraroldan@users.noreply.github.com>
2025-10-25 13:13:36 +0100

    Add a structure that can hold many files (#39)

--- src/source.rs
+++ src/source.rs
@@ -1,11 +1,14 @@
 use std::{
-    collections::HashMap,
+    collections::{BTreeSet, HashMap},
+    fmt::Debug,
     fs::File,
     io::{BufReader, Read},
+    path::{Path, PathBuf},
 };
 
 use ariadne::{Label, Report, ReportKind, Source};
 use chumsky::{Parser, container::Seq, input::Input};
+use clap::builder::PathBufValueParser;
 use proptest::collection::HashMapStrategy;
 
 use crate::{
@@ -22,19 +25,32 @@ pub struct FileId(pub usize);
 /// A Calamars source file
 pub struct SourceFile {
     pub id: FileId,
-    pub path: std::path::PathBuf,
+    pub path: PathBuf,
     pub src: String,
 }
 
-impl TryFrom<std::path::PathBuf> for SourceFile {
+impl TryFrom<PathBuf> for SourceFile {
     type Error = std::io::Error;
 
-    fn try_from(path: std::path::PathBuf) -> Result<Self, Self::Error> {
+    fn try_from(path: PathBuf) -> Result<Self, Self::Error> {
         let file = File::open(&path)?;
         let mut buf_reader = BufReader::new(file);
         let mut src = String::new();
         buf_reader.read_to_string(&mut src)?;
         let id = FileId(0);
+        Ok(SourceFile { id, path, src })
+    }
+}
+
+impl TryFrom<(usize, PathBuf)> for SourceFile {
+    type Error = std::io::Error;
+
+    fn try_from((id, path): (usize, PathBuf)) -> Result<Self, Self::Error> {
+        let file = File::open(&path)?;
+        let mut buf_reader = BufReader::new(file);
+        let mut src = String::new();
+        buf_reader.read_to_string(&mut src)?;
+        let id = FileId(id);
         Ok(SourceFile { id, path, src })
     }
 }
@@ -51,6 +67,10 @@ impl SourceFile {
 
     fn file_source(&self) -> &String {
         &self.src
+    }
+
+    fn path(&self) -> &PathBuf {
+        &self.path
     }
 
     /// Tokenize the file, and return the tokens as a TokenInput stream
@@ -96,5 +116,85 @@ impl SourceFile {
 
         let fin = rep_labelled.finish();
         fin.print((&file_name, Source::from(self.file_source())))
+    }
+}
+
+/// All of the projects files
+///
+/// Once this is created with `SourceDBBuilder`, it cannot be mutated (as it can break ordering of
+/// vector)
+pub struct SourceDB {
+    files: Vec<SourceFile>,
+}
+
+impl TryFrom<SourceDBBuilder> for SourceDB {
+    type Error = std::io::Error;
+
+    fn try_from(builder: SourceDBBuilder) -> Result<Self, Self::Error> {
+        let files = builder
+            .file_paths
+            .into_iter()
+            .enumerate()
+            .map(TryFrom::try_from)
+            .collect::<Result<Vec<SourceFile>, Self::Error>>()?;
+        Ok(Self { files })
+    }
+}
+
+impl SourceDB {
+    pub fn files(&self) -> &Vec<SourceFile> {
+        &self.files
+    }
+
+    pub fn get_file(&self, FileId(id): FileId) -> Option<&SourceFile> {
+        self.files.get(id)
+    }
+
+    pub fn get_path(&self, id: FileId) -> Option<&PathBuf> {
+        self.get_file(id).map(|file| &file.path)
+    }
+
+    pub fn len(&self) -> usize {
+        self.files.len()
+    }
+}
+
+#[derive(Default, Debug)]
+pub struct SourceDBBuilder {
+    file_paths: BTreeSet<PathBuf>,
+}
+
+impl SourceDBBuilder {
+    pub fn add_path(mut self, new_path: PathBuf) -> Self {
+        self.file_paths.insert(new_path);
+        self
+    }
+
+    pub fn finish(self) -> Result<SourceDB, std::io::Error> {
+        TryFrom::try_from(self)
+    }
+}
+
+#[cfg(test)]
+mod test_builder {
+    use std::{fs, path::PathBuf};
+
+    use crate::source::{FileId, SourceDBBuilder};
+
+    #[test]
+    fn load_many_files() {
+        let sourcedb = SourceDBBuilder::default()
+            .add_path("./tests/test_files/sample_file.cm".into())
+            .add_path("./tests/test_files/bad_function.cm".into())
+            .finish();
+
+        assert!(sourcedb.is_ok());
+        let sourcedb = sourcedb.unwrap();
+        assert_eq!(sourcedb.len(), 2);
+        assert_eq!(
+            sourcedb.get_file(FileId(0)).unwrap().path().clone(),
+            PathBuf::from("./tests/test_files/bad_function.cm"),
+            "Paths shuold be inserted in alphabetical order"
+        );
     }
 }
