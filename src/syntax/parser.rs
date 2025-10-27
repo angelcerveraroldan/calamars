c397a119b64c27900ddcc7400ace1bf76da2a75f
Angel Cervera Roldan <48255007+angelcerveraroldan@users.noreply.github.com>
2025-10-27 19:51:36 +0000

    Parse expressions

--- src/syntax/parser.rs
+++ src/syntax/parser.rs
@@ -1,7 +1,54 @@
 use crate::{
     source::FileId,
-    syntax::{ast, errors::ParsingError, span::Span, token::Token},
+    syntax::{
+        ast::{self, FuncCall},
+        errors::ParsingError,
+        span::Span,
+        token::Token,
+    },
 };
+
+fn uni_span(s: &Span) -> Span {
+    Span::from(s.start..s.start)
+}
+
+const UNARY_BP: usize = 90;
+const FUN_CALL: usize = 80;
+
+// Precedence information
+#[rustfmt::kip]
+const fn precedence(b: &Token) -> Option<(usize, usize)> {
+    match b {
+        Token::Pow => Some((70, 70)),
+        Token::Star | Token::Slash => Some((60, 61)),
+        Token::Plus | Token::Minus | Token::Concat => Some((50, 51)),
+        Token::GreaterEqual | Token::LessEqual => Some((40, 41)),
+        Token::EqualEqual | Token::NotEqual => Some((35, 36)),
+        Token::And => Some((30, 31)),
+        Token::Xor => Some((25, 26)),
+        Token::Or => Some((20, 21)),
+        _ => None,
+    }
+}
+
+const fn binary_op_from_token(token: &Token) -> Option<ast::BinaryOperator> {
+    match token {
+        Token::Xor => Some(ast::BinaryOperator::Xor),
+        Token::EqualEqual => Some(ast::BinaryOperator::EqEq),
+        Token::NotEqual => Some(ast::BinaryOperator::NotEqual),
+        Token::LessEqual => Some(ast::BinaryOperator::Leq),
+        Token::GreaterEqual => Some(ast::BinaryOperator::Geq),
+        Token::Concat => Some(ast::BinaryOperator::Concat),
+        Token::Plus => Some(ast::BinaryOperator::Add),
+        Token::Minus => Some(ast::BinaryOperator::Sub),
+        Token::Star => Some(ast::BinaryOperator::Times),
+        Token::Pow => Some(ast::BinaryOperator::Pow),
+        Token::Slash => Some(ast::BinaryOperator::Div),
+        Token::Less => Some(ast::BinaryOperator::Less),
+        Token::Greater => Some(ast::BinaryOperator::Greater),
+        _ => None,
+    }
+}
 
 pub struct CalamarsParser {
     /// Id for the file for which this parser is responsible
@@ -42,6 +89,13 @@ impl CalamarsParser {
         self.diag.push(err);
     }
 
+    fn expect_err(&mut self, expect: impl Into<String>) {
+        self.insert_err(ParsingError::Expected {
+            expected: expect.into(),
+            span: self.zero_width_here(),
+        });
+    }
+
     /// Get the index of the EOF token
     fn eof_index(&self) -> usize {
         self.tokens.len().saturating_sub(1)
@@ -49,6 +103,15 @@ impl CalamarsParser {
 
     fn at_end(&self) -> bool {
         self.n_index() == self.eof_index()
+    }
+
+    fn zero_width_here(&self) -> Span {
+        let span = self.next_ref().1;
+        Span::from(span.start..span.start)
+    }
+
+    fn last_consumed_span(&self) -> Span {
+        self.tokens[self.n_index().saturating_sub(1)].1
     }
 
     /// Get the index of the next non-consumed token
@@ -108,11 +171,13 @@ impl CalamarsParser {
     /// Keep skipping tokens until some predicate is met.
     ///
     /// This can be used for error recovery.
-    fn skip_to(&mut self, until: fn(&Token) -> bool) {
+    fn skip_until<Ctx>(&mut self, mut init: Ctx, until: impl Fn(&mut Ctx, &Token) -> bool) {
         loop {
-            if self.next_matches(until) || self.at_end() {
+            let next_token = self.next_token_ref();
+            if until(&mut init, next_token) || self.at_end() {
                 return;
             }
+
             self.advance_one();
         }
     }
@@ -137,8 +202,191 @@ impl CalamarsParser {
         }
     }
 
+    /// Try to parse a literal.
+    fn try_lit(&self) -> Option<ast::Literal> {
+        let (tk, span) = self.next_ref();
+        let lit_kind = match tk {
+            Token::True => ast::LiteralKind::Boolean(true),
+            Token::False => ast::LiteralKind::Boolean(false),
+            Token::Int(i) => ast::LiteralKind::Integer(*i),
+            Token::Float(f) => ast::LiteralKind::Real(*f),
+            Token::Char(c) => ast::LiteralKind::Char(*c),
+            Token::String(s) => ast::LiteralKind::String(s.clone()),
+            _ => return None,
+        };
+        Some(ast::Literal::new(lit_kind, *span))
+    }
+
+    // Literals, name/path, bracketed expressions
+    fn parse_primary(&mut self) -> ast::Expression {
+        // A literal is the simplest case
+        if let Some(literal) = self.try_lit() {
+            self.advance_one();
+            return ast::Expression::Literal(literal);
+        }
+        let (tk, sp) = self.next_ref().clone();
+        match tk {
+            // TODO: For now we just supprt idents to be one word
+            Token::Ident(_) => ast::Expression::Identifier(self.parse_identifier()),
+            Token::LParen => {
+                self.advance_one();
+                let expr = self.parse_expression();
+
+                // Try to find closing paren
+                // If we dont find it, we will artificially insert it
+                if !self.next_matches(|t| *t == Token::RParen) {
+                    self.insert_err(ParsingError::DelimeterNotClosed {
+                        opening_loc: sp,
+                        expected: ")",
+                        at: self.zero_width_here(),
+                    });
+                }
+
+                expr
+            }
+            // For no skip, not implemented
+            Token::LBrace => {
+                self.skip_until(1, |a, b| {
+                    if matches!(b, Token::RBrace) {
+                        *a -= 1;
+                    }
+                    if matches!(b, Token::LBrace) {
+                        *a += 1;
+                    }
+                    *a == 0
+                });
+                ast::Expression::Error(self.zero_width_here())
+            }
+            _ => {
+                let at = self.zero_width_here();
+                self.insert_err(ParsingError::Expected {
+                    expected: "expression".into(),
+                    span: at,
+                });
+                ast::Expression::Error(at)
+            }
+        }
+    }
+
+    /// Parse comma separated arguments.
+    ///
+    /// To call this, you must ensure that the first token is LParen.
+    fn parse_fn_arguments(&mut self) -> Vec<ast::Expression> {
+        let mut arguments = vec![];
+        self.advance_one();
+
+        loop {
+            // Handle the case where there are no arguments
+            if self.next_matches(|tk| *tk == Token::RParen) {
+                self.advance_one();
+                return arguments;
+            }
+
+            arguments.push(self.parse_expression());
+
+            if self.next_matches(|tk| *tk == Token::Comma) {
+                self.advance_one(); // Consume the comma
+            } else {
+                break;
+            }
+        }
+
+        if self.next_matches(|tk| *tk == Token::RParen) {
+            self.advance_one();
+        } else {
+            self.expect_err(")");
+        }
+
+        arguments
+    }
+
+    /// Given some expression, apply postfix operations to it.
+    ///
+    /// This will handle things such as `f(x)`. We will apply `(x)` to `f`.
+    fn parse_postfix_operation(&mut self, mut expr: ast::Expression) -> ast::Expression {
+        loop {
+            let (tk, sp) = self.next_ref().clone();
+            match tk {
+                // Apply parameters to input
+                Token::LParen => {
+                    let args = self.parse_fn_arguments();
+                    let span_end = self.last_consumed_span();
+                    let span = Span::from(sp.start..span_end.end);
+                    expr = ast::Expression::FunctionCall(FuncCall::new(expr, args, span));
+                }
+                // TODO: Index not yet supported
+                Token::LBracket => {
+                    self.skip_until(1, |a, b| {
+                        if matches!(b, Token::RBracket) {
+                            *a -= 1;
+                        }
+                        if matches!(b, Token::LBracket) {
+                            *a += 1;
+                        }
+                        *a == 0
+                    });
+                }
+                _ => break,
+            }
+        }
+        expr
+    }
+
+    fn parse_prefix(&mut self) -> ast::Expression {
+        if self.next_matches(|t| *t == Token::Minus) {
+            let unary_span = self.next_ref().1;
+            self.advance_one();
+            let expression = self.parse_expression_with_binding_power(UNARY_BP);
+            let span = Span::from(unary_span.start..expression.span().end);
+            let unary = ast::UnaryOp::new(ast::UnaryOperator::Neg, expression.into(), span);
+            ast::Expression::UnaryOp(unary)
+        } else {
+            self.parse_primary()
+        }
+    }
+
+    /// Pratt parsing for binary expressions
+    ///
+    /// `Expression <symbol> Expression` where <symbol> can be one of the following:
+    /// <, <=, >, >=, |>, and, or, xor, *, ^, /, +, -
+    fn parse_expression_with_binding_power(&mut self, binding_power: usize) -> ast::Expression {
+        let mut lhs = self.parse_prefix();
+        lhs = self.parse_postfix_operation(lhs);
+        loop {
+            let token = self.next_token_ref();
+            let operator = precedence(token);
+            if operator.is_none() {
+                break;
+            }
+
+            let (lbp, rbp) = operator.unwrap();
+            let operator = binary_op_from_token(token).unwrap();
+            if lbp < binding_power {
+                break;
+            }
+
+            // Consume the operator
+            self.advance_one();
+            let rhs = self.parse_expression_with_binding_power(rbp);
+            let rhs = self.parse_postfix_operation(rhs);
+            let span = Span::from(lhs.span().start..rhs.span().end);
+            let binop = ast::BinaryOp::new(operator, lhs.into(), rhs.into(), span);
+            lhs = ast::Expression::BinaryOp(binop);
+        }
+        lhs
+    }
+
+    fn parse_expression(&mut self) -> ast::Expression {
+        self.parse_expression_with_binding_power(0)
+    }
+
     /// Parse types
-    fn parse_type_path(&mut self) -> ast::Type {
+    ///
+    /// Possible types currently implemented are:
+    /// - Path (something like `String`, or `foo.Bar`)
+    /// - Function (`Type -> Type`)
+    /// - Array (`[ Type ]`)
+    fn parse_type(&mut self) -> ast::Type {
         todo!()
     }
 
@@ -154,13 +402,110 @@ impl CalamarsParser {
 }
 
 #[cfg(test)]
-mod test_parser {
-    use crate::syntax::token::Token;
+mod tests {
+    use super::*;
 
-    fn identifier() -> Token {
-        Token::Ident("Hello".into())
+    fn sp(start: usize, end: usize) -> logos::Span {
+        logos::Span { start, end }
+    }
+
+    fn toks(pairs: &[(Token, (usize, usize))]) -> Vec<(Token, logos::Span)> {
+        pairs
+            .iter()
+            .map(|(t, (s, e))| (t.clone(), sp(*s, *e)))
+            .collect()
+    }
+
+    fn parse_expr_from_tokens(
+        tokens: Vec<(Token, logos::Span)>,
+    ) -> (CalamarsParser, ast::Expression) {
+        let file = FileId(0);
+        let mut p = CalamarsParser::new(file, tokens);
+        let expr = p.parse_expression();
+        (p, expr)
     }
 
     #[test]
-    fn parse_ident() {}
+    fn expr_precedence_mul_over_add() {
+        // 1 + 2 * 3
+        let tokens = toks(&[
+            (Token::Int(1), (0, 1)),
+            (Token::Plus, (2, 3)),
+            (Token::Int(2), (4, 5)),
+            (Token::Star, (6, 7)),
+            (Token::Int(3), (8, 9)),
+            (Token::EOF, (9, 9)),
+        ]);
+        let (p, _e) = parse_expr_from_tokens(tokens);
+        assert!(p.diag.is_empty(), "should parse without diagnostics");
+    }
+
+    #[test]
+    fn expr_calls_chain() {
+        // f(1)(2 + 3)
+        let tokens = toks(&[
+            (Token::Ident("f".into()), (0, 1)),
+            (Token::LParen, (1, 2)),
+            (Token::Int(1), (2, 3)),
+            (Token::RParen, (3, 4)),
+            (Token::LParen, (4, 5)),
+            (Token::Int(2), (5, 6)),
+            (Token::Plus, (7, 8)),
+            (Token::Int(3), (9, 10)),
+            (Token::RParen, (10, 11)),
+            (Token::EOF, (11, 11)),
+        ]);
+        let (p, _e) = parse_expr_from_tokens(tokens);
+        assert!(p.diag.is_empty(), "call chaining should parse cleanly");
+    }
+
+    #[test]
+    fn expr_grouping_then_mul() {
+        // (a + b) * c
+        let tokens = toks(&[
+            (Token::LParen, (0, 1)),
+            (Token::Ident("a".into()), (1, 2)),
+            (Token::Plus, (3, 4)),
+            (Token::Ident("b".into()), (5, 6)),
+            (Token::RParen, (6, 7)),
+            (Token::Star, (8, 9)),
+            (Token::Ident("c".into()), (10, 11)),
+            (Token::EOF, (11, 11)),
+        ]);
+        let (p, _e) = parse_expr_from_tokens(tokens);
+        assert!(
+            p.diag.is_empty(),
+            "grouping + precedence should parse without diagnostics"
+        );
+    }
+
+    #[test]
+    fn expr_missing_rparen_in_call_emits_diag() {
+        // f(1
+        let tokens = toks(&[
+            (Token::Ident("f".into()), (0, 1)),
+            (Token::LParen, (1, 2)),
+            (Token::Int(1), (2, 3)),
+            (Token::EOF, (3, 3)),
+        ]);
+        let (p, _e) = parse_expr_from_tokens(tokens);
+        assert!(!p.diag.is_empty(), "should report a missing `)` diagnostic");
+    }
+
+    #[test]
+    fn expr_missing_rhs_after_plus_emits_diag() {
+        // 1 +
+        let tokens = toks(&[
+            (Token::Int(1), (0, 1)),
+            (Token::Plus, (2, 3)),
+            (Token::EOF, (3, 3)),
+        ]);
+        let (mut p, _e) = parse_expr_from_tokens(tokens);
+        // Force another parse to ensure recovery doesn't loop (optional)
+        let _ = p.parse_expression();
+        assert!(
+            !p.diag.is_empty(),
+            "should report an expected expression after `+`"
+        );
+    }
 }
