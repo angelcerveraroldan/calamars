d576e4631866d12e0173b56bcd5dde2e27f94ebc
Angel Cervera Roldan <48255007+angelcerveraroldan@users.noreply.github.com>
2025-10-27 19:51:41 +0000

    Parser errors

--- src/syntax/errors.rs
+++ src/syntax/errors.rs
@@ -1,5 +1,13 @@
 use crate::syntax::span::Span;
 
 pub enum ParsingError {
-    Expected { expected: String, span: Span },
+    Expected {
+        expected: String,
+        span: Span,
+    },
+    DelimeterNotClosed {
+        expected: &'static str,
+        at: Span,
+        opening_loc: Span,
+    },
 }
