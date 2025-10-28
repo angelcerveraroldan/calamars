48493b530515758ef6d37f221027811217796498
Angel Cervera Roldan <48255007+angelcerveraroldan@users.noreply.github.com>
2025-10-28 22:10:21 +0000

    Generalize errors

--- src/errors.rs
+++ src/errors.rs
@@ -1,1 +1,41 @@
+use ariadne::{Color, Label, Report, ReportKind, Source};
+use std::ops::Range;
 
+use crate::syntax::span::Span;
+
+pub fn label_from(
+    file_name: &String,
+    span: Span,
+    message: impl Into<String>,
+    color: Option<Color>,
+) -> Label<(&String, Range<usize>)> {
+    let mut label = Label::new((file_name, span.into_range())).with_message(message.into());
+    if let Some(color) = color {
+        label = label.with_color(color);
+    }
+    label
+}
+
+pub trait PrettyError {
+    fn labels<'a>(&'a self, file_name: &'a String) -> Vec<Label<(&'a String, Range<usize>)>>;
+    fn notes(&self) -> Option<String>;
+    fn message(&self) -> &str;
+
+    /// Given a semantic error, pretty print it with the source code
+    fn log_error(&self, file_name: &String, source: &String) -> Result<(), std::io::Error> {
+        let rep = Report::build(ReportKind::Error, (file_name, 12..12))
+            .with_message(self.message().to_string());
+
+        let mut rep_labelled = self
+            .labels(file_name)
+            .into_iter()
+            .fold(rep, |rep, label| rep.with_label(label));
+
+        if let Some(note) = self.notes() {
+            rep_labelled = rep_labelled.with_note(note);
+        };
+
+        let fin = rep_labelled.finish();
+        fin.print((file_name, Source::from(source)))
+    }
+}
--- src/syntax/errors.rs
+++ src/syntax/errors.rs
@@ -1,4 +1,9 @@
-use crate::syntax::span::Span;
+use ariadne::Color;
+
+use crate::{
+    errors::{PrettyError, label_from},
+    syntax::span::Span,
+};
 
 #[derive(Debug, Clone)]
 pub enum ParsingError {
@@ -11,4 +16,66 @@ pub enum ParsingError {
         at: Span,
         opening_loc: Span,
     },
+}
+
+impl PrettyError for ParsingError {
+    fn labels<'a>(
+        &'a self,
+        file_name: &'a String,
+    ) -> Vec<ariadne::Label<(&'a String, std::ops::Range<usize>)>> {
+        match self {
+            ParsingError::Expected { expected, span } => {
+                vec![label_from(
+                    file_name,
+                    *span,
+                    format!("Expected to find: {expected}"),
+                    Some(Color::Red),
+                )]
+            }
+            ParsingError::DelimeterNotClosed {
+                expected,
+                at,
+                opening_loc,
+            } => vec![
+                label_from(
+                    file_name,
+                    *opening_loc,
+                    "Opening delimeter here",
+                    Some(Color::Green),
+                ),
+                label_from(file_name, *at, "No closing delimeter", Some(Color::Red)),
+            ],
+        }
+    }
+
+    fn notes(&self) -> Option<String> {
+        match self {
+            ParsingError::Expected { expected, span } => None,
+            ParsingError::DelimeterNotClosed { .. } => None,
+        }
+    }
+
+    fn message(&self) -> &str {
+        match self {
+            ParsingError::Expected { .. } => "Did not find expected token",
+            ParsingError::DelimeterNotClosed { .. } => "Delimeter not closed",
+        }
+    }
+
+    fn log_error(&self, file_name: &String, source: &String) -> Result<(), std::io::Error> {
+        let rep = ariadne::Report::build(ariadne::ReportKind::Error, (file_name, 12..12))
+            .with_message(self.message().to_string());
+
+        let mut rep_labelled = self
+            .labels(file_name)
+            .into_iter()
+            .fold(rep, |rep, label| rep.with_label(label));
+
+        if let Some(note) = self.notes() {
+            rep_labelled = rep_labelled.with_note(note);
+        };
+
+        let fin = rep_labelled.finish();
+        fin.print((file_name, ariadne::Source::from(source)))
+    }
 }
