ea56a55d30f2a5bf4a42bd609b3503139967c04a
Angel Cervera Roldan <48255007+angelcerveraroldan@users.noreply.github.com>
2025-11-01 20:38:21 +0000

    Generate IDs and Arenas + Interning

---
+++ calamars_core/src/ids.rs
@@ -0,0 +1,22 @@
+use paste::paste;
+
+/// Generate Ids for given identifiers.
+macro_rules! id_gen {
+    ($($name:ident),*$(,)?) => {
+        $(
+            paste! {
+                #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+                pub struct [<$name Id>](usize);
+                impl [<$name Id>] {
+                    pub fn inner(&self) -> usize { self.0 }
+                }
+                impl From<usize> for [<$name Id>] { fn from(u: usize) -> Self { Self(u) } }
+                impl crate::Identifier for [<$name Id>] {
+                    fn inner_id(&self) -> usize { self.inner() }
+                }
+            }
+        )*
+    };
+}
+
+id_gen!(Type, Symbol, Expression, Ident, String);
