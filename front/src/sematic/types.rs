fbed075a77994382f9797aa5bf44c1427c9383bc
Angel Cervera Roldan <48255007+angelcerveraroldan@users.noreply.github.com>
2025-11-08 23:01:47 +0000

    Basic type checking

--- front/src/sematic/types.rs
+++ front/src/sematic/types.rs
@@ -8,22 +8,65 @@ use calamars_core::{
 use crate::{
     sematic::{
         error::SemanticError,
-        hir::{self, ExpressionArena, IdentArena, SymbolArena, Type, TypeArena},
+        hir::{self, ExpressionArena, IdentArena, SymbolArena, Type, TypeArena, type_id_stringify},
     },
     syntax::{ast::Expression, span::Span},
 };
 
-pub struct TypeHandler {
+/// Immutable context containig arenas from Stage 1
+pub struct Context {
     symbols: SymbolArena,
     expressions: ExpressionArena,
-    types: TypeArena,
+}
 
+pub struct TypeHandler {
+    types: TypeArena,
     errors: Vec<SemanticError>,
 }
 
 impl TypeHandler {
-    fn type_expression(&mut self, e_id: ids::ExpressionId) -> ids::TypeId {
-        let expression = self.expressions.get_unchecked(e_id);
+    fn match_type(&mut self, id: ids::TypeId, ty: &Type) -> bool {
+        let ty_id = self.types.intern(ty);
+        id == ty_id
+    }
+
+    #[inline]
+    fn push_wrong_type(&mut self, actual: ids::TypeId, expected: &str, span: Span) {
+        self.errors.push(SemanticError::WrongType {
+            actual: type_id_stringify(&self.types, actual),
+            expected: expected.into(),
+            span,
+        });
+    }
+
+    fn ensure_numeric(&mut self, cx: &Context, expr: ids::ExpressionId, t: ids::TypeId) {
+        if self.match_type(t, &Type::Integer) || self.match_type(t, &Type::Float) {
+            return;
+        }
+        let sp = cx.expressions.get_unchecked(expr).get_span().unwrap();
+        self.push_wrong_type(t, "Numerical", sp);
+    }
+
+    fn ensure_type(
+        &mut self,
+        ctx: &Context,
+        expr: ids::ExpressionId,
+        ty_id: ids::TypeId,
+        expected: ids::TypeId,
+    ) {
+        if ty_id == expected {
+            return;
+        }
+        let sp = ctx.expressions.get_unchecked(expr).get_span().unwrap();
+        self.push_wrong_type(ty_id, &type_id_stringify(&self.types, expected), sp);
+    }
+
+    /// Given some expression, this will return it's type id. If there are any semantic typing
+    /// errors, for example: `2 + "hello"` they will be added to the `errors` vector, and will
+    /// return the typeid of the `Error` type.
+    fn type_expression(&mut self, ctx: &Context, e_id: &ids::ExpressionId) -> ids::TypeId {
+        let expression = ctx.expressions.get_unchecked(*e_id);
+
         match expression {
             super::hir::Expr::Err => self.types.err_id(),
             super::hir::Expr::Literal { constant, .. } => {
@@ -34,7 +77,7 @@ impl TypeHandler {
                 };
                 *self.types.resolve_unchecked(&ty)
             }
-            super::hir::Expr::Identifier { id, .. } => self
+            super::hir::Expr::Identifier { id, .. } => ctx
                 .symbols
                 .get(*id)
                 .map(|s| s.ty_id())
@@ -44,9 +87,9 @@ impl TypeHandler {
                 lhs,
                 rhs,
                 span,
-            } => self.type_check_binary_ops(operator, lhs, rhs, *span),
+            } => self.type_check_binary_ops(ctx, operator, lhs, rhs, *span),
             super::hir::Expr::Call { f, inputs, span } => {
-                let expression_ty = self.type_expression(*f);
+                let expression_ty = self.type_expression(ctx, f);
                 // TODO: Handle error type separately here
                 match self.types.get_unchecked(expression_ty) {
                     Type::Function { output, .. } => *output,
@@ -64,11 +107,46 @@ impl TypeHandler {
 
     fn type_check_binary_ops(
         &mut self,
+        ctx: &Context,
         op: &hir::BinOp,
         lhs: &ExpressionId,
         rhs: &ExpressionId,
         span: Span,
     ) -> ids::TypeId {
-        todo!()
+        let lhs_type_id = self.type_expression(ctx, lhs);
+        let rhs_type_id = self.type_expression(ctx, rhs);
+
+        let int_type_id = *self.types.resolve_unchecked(&Type::Integer);
+        let float_type_id = *self.types.resolve_unchecked(&Type::Float);
+
+        match op {
+            // Both numerical
+            hir::BinOp::Add | hir::BinOp::Sub | hir::BinOp::Mult | hir::BinOp::Div => {
+                self.ensure_numeric(ctx, *lhs, lhs_type_id);
+                self.ensure_numeric(ctx, *rhs, rhs_type_id);
+
+                let lhs_numerical = (lhs_type_id == float_type_id) || (lhs_type_id == int_type_id);
+                let rhs_numerical = (rhs_type_id == float_type_id) || (rhs_type_id == int_type_id);
+
+                // If they are not both numerica, then this is an error
+                if !(lhs_numerical && rhs_numerical) {
+                    return self.types.err_id();
+                }
+
+                // If they are both integers, then we will return integer
+                if (lhs_type_id == int_type_id && rhs_type_id == int_type_id) {
+                    return int_type_id;
+                }
+
+                // Both floats, or one float, then we cast to float
+                float_type_id
+            }
+            // Both integers
+            hir::BinOp::Mod => {
+                self.ensure_type(ctx, *lhs, lhs_type_id, int_type_id);
+                self.ensure_type(ctx, *rhs, rhs_type_id, int_type_id);
+                self.types.err_id()
+            }
+        }
     }
 }
