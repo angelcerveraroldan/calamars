7d73be06ccf70269be27fc26759918fe05b8cac4
Angel Cervera Roldan <48255007+angelcerveraroldan@users.noreply.github.com>
2025-11-21 16:54:21 +0000

    Add failing test

---
+++ front/tests/tokparse.rs
@@ -0,0 +1,31 @@
+use calamars_core::ids::FileId;
+use front::syntax::{
+    ast::{Declaration, Expression, Item},
+    token::Token,
+};
+
+#[test]
+fn parse_fn_without_spaces() {
+    let source = "def yyy() = 2+1";
+    let tokens = Token::tokens_spanned_stream(source);
+
+    let mut parser = front::syntax::parser::CalamarsParser::new(FileId::from(0), tokens);
+    let item = parser.parse_item();
+
+    assert!(
+        parser.diag().is_empty(),
+        "There shuold be no errors parsing"
+    );
+
+    let f = match item {
+        Item::Declaration(Declaration::Function(fd)) => fd,
+        _ => panic!("We should have parsed a function"),
+    };
+
+    let body = f.body();
+
+    assert!(
+        matches!(body, Expression::BinaryOp(_)),
+        "Expression assigned should be a binary op"
+    );
+}
