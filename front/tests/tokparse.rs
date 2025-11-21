1420b5943a65ffa393bd29c2114b317ab88d8c49
Angel Cervera Roldan <48255007+angelcerveraroldan@users.noreply.github.com>
2025-11-21 16:54:56 +0000

    Add passing function

--- front/tests/tokparse.rs
+++ front/tests/tokparse.rs
@@ -29,3 +29,29 @@ fn parse_fn_without_spaces() {
         "Expression assigned should be a binary op"
     );
 }
+
+#[test]
+fn parse_fn_with_spaces() {
+    let source = "def yyy() = 2 + 1";
+    let tokens = Token::tokens_spanned_stream(source);
+
+    let mut parser = front::syntax::parser::CalamarsParser::new(FileId::from(0), tokens);
+    let item = parser.parse_item();
+
+    assert!(
+        parser.diag().is_empty(),
+        "There shuold be no errors parsing"
+    );
+
+    let f = match item {
+        Item::Declaration(Declaration::Function(fd)) => fd,
+        _ => panic!("We should have parsed a function"),
+    };
+
+    let body = f.body();
+
+    assert!(
+        matches!(body, Expression::BinaryOp(_)),
+        "Expression assigned should be a binary op"
+    );
+}
