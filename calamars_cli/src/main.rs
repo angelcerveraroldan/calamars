bdd41110c0be05efb0f1cc686dabae348ae54bd6
Angel Cervera Roldan <48255007+angelcerveraroldan@users.noreply.github.com>
2026-01-24 22:13:59 +0000

    Run module with new API

--- calamars_cli/src/main.rs
+++ calamars_cli/src/main.rs
@@ -100,7 +100,13 @@ fn main() {
 
             if run_vm {
                 let mut vmlower = vm::Lowerer::new(&irmodule);
-                println!("{:?}", vmlower.run_module());
+                let vm = vmlower.finish().map_err(|err| {
+                    format!(
+                        "Failed to lower from MIR to VM Bytecode with error: {:?}",
+                        err
+                    )
+                }).unwrap();
+                vm.run_module();
             }
         }
     }
