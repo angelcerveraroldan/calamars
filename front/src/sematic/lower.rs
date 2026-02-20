f42b3f88d2b74815cf7190f488266a0f30f900cb
Angel Cervera Roldan <48255007+angelcerveraroldan@users.noreply.github.com>
2026-02-20 15:02:33 +0000

    Add symbols to the scope on typedef

--- front/src/sematic/lower.rs
+++ front/src/sematic/lower.rs
@@ -5,7 +5,7 @@ use calamars_core::ids;
 use crate::{
     sematic::{
         error::SemanticError,
-        hir::{self, SymbolBuilder, SymbolDec},
+        hir::{self, Symbol, SymbolBuilder, SymbolDec},
     },
     syntax::{
         ast::{self, Declaration},
@@ -92,8 +92,245 @@ impl HirBuilder {
         self.identifiers.intern(&identifier_name)
     }
 
-    fn lower_expression(&mut self, expr: &ast::Expression) -> ids::ExpressionId {
-        todo!()
+    fn lower_expression(
+        &mut self,
+        expr: &ast::Expression,
+        global_ctx: &mut hir::GlobalContext,
+    ) -> ids::ExpressionId {
+        let lowered = self.lower_expression_inner(expr, global_ctx);
+        self.expressions.push(lowered)
+    }
+
+    fn lower_expression_inner(
+        &mut self,
+        expr: &ast::Expression,
+        global_ctx: &mut hir::GlobalContext,
+    ) -> hir::Expr {
+        match expr {
+            ast::Expression::Literal(literal) => self.lower_literal(literal),
+            ast::Expression::Identifier(ident) => self.lower_identifier_expr(ident),
+            ast::Expression::BinaryOp(binary_op) => self.lower_binary_expr(binary_op, global_ctx),
+            ast::Expression::Apply(apply) => self.lower_apply_expr(apply, global_ctx),
+            ast::Expression::IfStm(ifstm) => self.lower_if_expr(ifstm, global_ctx),
+            ast::Expression::Block(block) => self.lower_block(block, global_ctx),
+            other => {
+                self.insert_error(SemanticError::NotSupported {
+                    msg: "Expression not yet supported",
+                    span: other.span(),
+                });
+                hir::Expr::Err
+            }
+        }
+    }
+
+    fn lower_block(
+        &mut self,
+        block: &ast::CompoundExpression,
+        global_ctx: &mut hir::GlobalContext,
+    ) -> hir::Expr {
+        self.push_scope();
+
+        let mut items = vec![];
+        let mut builders = hashbrown::HashMap::new();
+        for item in block.items.iter() {
+            match item {
+                ast::Item::Declaration(decl) => match decl {
+                    Declaration::TypeSignature {
+                        docs: _,
+                        name,
+                        dtype,
+                    } => {
+                        let sb = self.lower_type_declaration(dtype, name, global_ctx);
+                        let old = builders.insert(sb.ident_id(), sb);
+                        if let Some(original) = old {
+                            self.insert_error(SemanticError::Redeclaration {
+                                original_span: original.span_name_type,
+                                redec_span: name.span(),
+                            })
+                        }
+                    }
+                    Declaration::Binding { name, params, body } => {
+                        let name_id = self.lower_ident(name);
+                        let builder = builders.remove(&name_id);
+                        let Some(builder) = builder else {
+                            self.insert_error(SemanticError::TypeMissing {
+                                for_identifier: name.span(),
+                            });
+                            continue;
+                        };
+
+                        match self
+                            .lower_binding_declaration(name, params, body, builder, global_ctx)
+                        {
+                            Ok(symbol_id) => {
+                                self.insert_symbol_to_current_scope(
+                                    name_id,
+                                    symbol_id,
+                                    name.span(),
+                                );
+                                items.push(hir::ItemId::Symbol(symbol_id));
+                            }
+                            Err(err) => self.insert_error(err),
+                        }
+                    }
+                },
+                ast::Item::Expression(expr) => {
+                    let expr_id = self.lower_expression(expr, global_ctx);
+                    items.push(hir::ItemId::Expr(expr_id));
+                }
+            }
+        }
+
+        for (_, builder) in builders {
+            self.insert_error(SemanticError::MissingDeclaration {
+                name: builder.name,
+                type_declaration: builder.span_name_type,
+            });
+        }
+
+        let final_expr = block
+            .final_expr
+            .as_ref()
+            .map(|expr| self.lower_expression(expr, global_ctx));
+
+        self.pop_scope();
+        hir::Expr::Block {
+            items: items.into(),
+            final_expr,
+            span: block.total_span(),
+        }
+    }
+
+    fn lower_literal(&mut self, literal: &ast::Literal) -> hir::Expr {
+        let constant = match literal.kind() {
+            ast::LiteralKind::Integer(i) => hir::Const::I64(*i),
+            ast::LiteralKind::Boolean(b) => hir::Const::Bool(*b),
+            ast::LiteralKind::String(_s) => {
+                self.insert_error(SemanticError::NotSupported {
+                    msg: "String literals not yet",
+                    span: literal.span(),
+                });
+                return hir::Expr::Err;
+            }
+            _ => return hir::Expr::Err,
+        };
+        hir::Expr::Literal {
+            constant,
+            span: literal.span(),
+        }
+    }
+
+    fn resolve(&self, ident_id: ids::IdentId, span: Span) -> Result<ids::SymbolId, SemanticError> {
+        for scope in self.scopes.iter().rev() {
+            if let Some(id) = scope.get(&ident_id) {
+                return Ok(*id);
+            }
+        }
+        // We know that the identifier string must exist (we must have found it to call this
+        // function)
+        let name = self.identifiers.get_unchecked(ident_id).clone();
+        Err(SemanticError::IdentNotFound { name, span })
+    }
+
+    fn insert_symbol_to_current_scope(
+        &mut self,
+        ident: ids::IdentId,
+        symbol_id: ids::SymbolId,
+        span: Span,
+    ) -> Result<(), SemanticError> {
+        // Not sure how to recover here
+        let scope = self.scopes.last_mut().ok_or(SemanticError::InternalError {
+            msg: "Lowering to HIR, scopes is empty",
+            span,
+        })?;
+        // If the name was already in use, then we will log an error
+        if let Some(original) = scope.insert(ident, symbol_id) {
+            let original = self.symbols.get_unchecked(original);
+            self.insert_error(SemanticError::Redeclaration {
+                original_span: original.span_name_decl,
+                redec_span: span,
+            });
+        }
+        Ok(())
+    }
+
+    fn lower_identifier_expr(&mut self, ident: &ast::Ident) -> hir::Expr {
+        let span = ident.span();
+        let name = ident.ident();
+        let ident_id = self.identifiers.intern(&name.to_string());
+        match self.resolve(ident_id, span) {
+            Ok(id) => hir::Expr::Identifier { id, span },
+            Err(err) => {
+                self.insert_error(err);
+                hir::Expr::Err
+            }
+        }
+    }
+
+    fn lower_binary_expr(
+        &mut self,
+        binary_op: &ast::BinaryOp,
+        global_ctx: &mut hir::GlobalContext,
+    ) -> hir::Expr {
+        let operator = match binary_op.operator() {
+            ast::BinaryOperator::Add => hir::BinOp::Add,
+            ast::BinaryOperator::Sub => hir::BinOp::Sub,
+            ast::BinaryOperator::Times => hir::BinOp::Mult,
+            ast::BinaryOperator::Div => hir::BinOp::Div,
+            ast::BinaryOperator::EqEq => hir::BinOp::EqEq,
+            ast::BinaryOperator::NotEqual => hir::BinOp::NotEqual,
+            ast::BinaryOperator::Mod => hir::BinOp::Mod,
+            ast::BinaryOperator::Greater => hir::BinOp::Greater,
+            ast::BinaryOperator::Geq => hir::BinOp::Geq,
+            ast::BinaryOperator::Less => hir::BinOp::Less,
+            ast::BinaryOperator::Leq => hir::BinOp::Leq,
+            ast::BinaryOperator::Or => hir::BinOp::Or,
+            ast::BinaryOperator::Xor => hir::BinOp::Xor,
+            ast::BinaryOperator::And => hir::BinOp::And,
+            _ => return hir::Expr::Err,
+        };
+        let lhs = self.lower_expression(binary_op.lhs(), global_ctx);
+        let rhs = self.lower_expression(binary_op.rhs(), global_ctx);
+        hir::Expr::BinaryOperation {
+            operator,
+            lhs,
+            rhs,
+            span: binary_op.span(),
+        }
+    }
+
+    fn lower_apply_expr(
+        &mut self,
+        apply: &ast::Apply,
+        global_ctx: &mut hir::GlobalContext,
+    ) -> hir::Expr {
+        let f = self.lower_expression(apply.callable(), global_ctx);
+        let input_expr = apply.input();
+        let input = self.lower_expression(&input_expr, global_ctx);
+        hir::Expr::Call {
+            f,
+            input,
+            span: apply.span(),
+        }
+    }
+
+    fn lower_if_expr(
+        &mut self,
+        ifstm: &ast::IfStm,
+        global_ctx: &mut hir::GlobalContext,
+    ) -> hir::Expr {
+        let predicate = self.lower_expression(ifstm.pred().as_ref(), global_ctx);
+        let then = self.lower_expression(ifstm.then_expr().as_ref(), global_ctx);
+        let otherwise = self.lower_expression(ifstm.else_expr().as_ref(), global_ctx);
+        hir::Expr::If {
+            predicate,
+            then,
+            otherwise,
+            span: ifstm.span(),
+            pred_span: ifstm.pred_span(),
+            then_span: ifstm.then_span(),
+            othewise_span: ifstm.else_span(),
+        }
     }
 
     fn lower_type(
@@ -144,7 +381,31 @@ impl HirBuilder {
         let lowered_ty = self.lower_type(declared_type, global_context);
         let name_span = declared_name.span();
         let lowered_id = self.lower_ident(declared_name);
-        hir::SymbolBuilder::new(lowered_ty, lowered_id, name_span, None, None)
+        let err_expr =
+            self.lower_expression(&ast::Expression::Error(Span::from(0..0)), global_context);
+        let reserved = self.symbols.push(Symbol {
+            ty: lowered_ty,
+            name: lowered_id,
+            span_name_type: name_span,
+            span_name_decl: Span::from(0..0),
+            symbol_declaration: SymbolDec {
+                inputs: Box::new([]),
+                body: err_expr,
+            },
+        });
+        self.insert_symbol_to_current_scope(lowered_id, reserved, name_span);
+        hir::SymbolBuilder::new(lowered_ty, lowered_id, name_span, None, None, reserved)
+    }
+
+    fn finish_builder(
+        &mut self,
+        builder: SymbolBuilder,
+        global_ctx: &mut hir::GlobalContext,
+    ) -> Result<ids::SymbolId, SemanticError> {
+        let symbol = self.symbols.get_unchecked_mut(builder.reserved_spot);
+        symbol.span_name_decl = builder.span_name_decl.unwrap();
+        symbol.symbol_declaration = builder.symbol_declaration.unwrap();
+        Ok(builder.reserved_spot)
     }
 
     fn lower_binding_declaration(
@@ -153,14 +414,15 @@ impl HirBuilder {
         params: &Vec<ast::Ident>,
         body: &ast::Expression,
         mut builder: SymbolBuilder,
-    ) -> Result<hir::Symbol, SemanticError> {
+        global_ctx: &mut hir::GlobalContext,
+    ) -> Result<ids::SymbolId, SemanticError> {
         let inputs: Box<[ids::IdentId]> =
             params.iter().map(|ident| self.lower_ident(ident)).collect();
-        let body = self.lower_expression(body);
+        let body = self.lower_expression(body, global_ctx);
 
         builder.span_name_decl = Some(name.span());
         builder.symbol_declaration = Some(SymbolDec { inputs, body });
-        builder.finish()
+        self.finish_builder(builder, global_ctx)
     }
 
     /// Given some module, lower it and return any errors that were generated along the way
@@ -196,7 +458,7 @@ impl HirBuilder {
             let builder = builders.remove(&name_id);
 
             if let Some(builder) = builder {
-                self.lower_binding_declaration(name, params, body, builder);
+                self.lower_binding_declaration(name, params, body, builder, global_ctx);
             } else {
                 self.insert_error(SemanticError::TypeMissing {
                     for_identifier: name.span(),
