0aaca7b86a24e37a0839202bfd2962fef4174765
angelcerveraroldan <48255007+angelcerveraroldan@users.noreply.github.com>
2026-07-26 13:45:25 +0100

    refactor/front: add frontend context
    With the goal of no longer passing around and mutating one large
    global context across all stages of the compiler, we will create
    smaller stage-specific contexts.
    
    This commit creates a front-end context.

--- calamars_core/src/global.rs
+++ calamars_core/src/global.rs
@@ -6,6 +6,18 @@ use crate::{
     types::TypeArena,
 };
 
+pub struct FrontendCtx<'a> {
+    pub types: &'a mut TypeArena,
+    pub data_structs: &'a mut DStructArena,
+    pub struct_defs: &'a mut StructDefArena,
+    pub strings: &'a mut StringArena,
+}
+
+pub struct TypeCtx<'a> {
+    pub types: &'a mut TypeArena,
+    pub struct_defs: &'a StructDefArena,
+}
+
 /// Context and information that needs to be filled and
 /// shared between many stages of the compiler
 pub struct GlobalContext {
@@ -15,4 +27,22 @@ pub struct GlobalContext {
     pub strings: StringArena,
     pub memlay: MemoryLayoutArena,
     pub struct_mem: hashbrown::HashMap<ids::DStructId, ids::MemLayoutId>,
+}
+
+impl GlobalContext {
+    pub fn frontend(&mut self) -> FrontendCtx<'_> {
+        FrontendCtx {
+            types: &mut self.types,
+            data_structs: &mut self.data_structs,
+            struct_defs: &mut self.struct_defs,
+            strings: &mut self.strings,
+        }
+    }
+
+    pub fn type_ctx(&mut self) -> TypeCtx<'_> {
+        TypeCtx {
+            types: &mut self.types,
+            struct_defs: &self.struct_defs,
+        }
+    }
 }
