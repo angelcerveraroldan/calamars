ccdcc2bcc5b0b1bfdd30ddadf6b4a1c73952be2f
angelcerveraroldan <angelcerveraroldan@protonmail.com>
2026-09-11 08:45:35 +0100

    mir/mdata: preserve a little more information for field access

--- mir/src/mdata.rs
+++ mir/src/mdata.rs
@@ -13,36 +13,27 @@ use front::semantic::hir;
 pub type FieldIndex = usize;
 pub type FieldName = String;
 
+pub struct FieldInfo {
+    pub name: String,
+    pub ftype: ids::TypeId,
+    pub index: FieldIndex,
+}
+
 #[derive(Default)]
 pub struct MirData {
     /// Given some data structure's field name, find its index
-    struct_indices: hashbrown::HashMap<(DStructId, FieldName), FieldIndex>,
+    struct_indices: hashbrown::HashMap<(DStructId, FieldName), FieldInfo>,
 }
 
 impl MirData {
     pub fn get_field_index_by_name(&self, dstructid: &DStructId, name: &str) -> Option<FieldIndex> {
         self.struct_indices
             .get(&(*dstructid, name.to_string()))
-            .copied()
+            .map(|field| field.index)
     }
 
-    /// Given the HIR data of a struct, insert the data into the
-    /// MirData table
-    fn generate_struct_indices(
-        &mut self,
-        dstructid: ids::DStructId,
-        ordered_fields: impl Iterator<Item = String>,
-    ) -> crate::lower::MirRes<()> {
-        for (index, field) in ordered_fields.enumerate() {
-            let k = self
-                .struct_indices
-                .insert((dstructid, field.clone()), index);
-            // This should never happen, if it does, then there is an
-            // issue in the front end
-            debug_assert!(k.is_none(), "Struct had the same key more than once");
-        }
-
-        Ok(())
+    pub fn get_field_info_by_name(&self, dstructid: &DStructId, name: &str) -> Option<&FieldInfo> {
+        self.struct_indices.get(&(*dstructid, name.to_string()))
     }
 
     fn generate_struct_data(
@@ -50,8 +41,20 @@ impl MirData {
         dstructid: &DStructId,
         structure: &StructDef,
     ) -> crate::lower::MirRes<()> {
-        let ordered_fields = structure.fields.iter().map(|field| field.name.clone());
-        self.generate_struct_indices(*dstructid, ordered_fields)
+        for (index, field) in structure.fields.iter().enumerate() {
+            let k = self.struct_indices.insert(
+                (*dstructid, field.name.clone()),
+                FieldInfo {
+                    name: field.name.clone(),
+                    ftype: field.ty,
+                    index,
+                },
+            );
+            // This should never happen, if it does, then there is an
+            // issue in the front end
+            debug_assert!(k.is_none(), "Struct had the same key more than once");
+        }
+        Ok(())
     }
 
     pub fn generate_mirdata<'a>(
